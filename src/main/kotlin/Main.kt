bbc27a38d29b6fdea1a4e1bc3b5b848a37d5945a
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-03-07 02:16:13 +0300

    Add init var

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package vorobev
+
+fun main() {
+    println("Hello World!")
+}
---
+++ src/main/kotlin/basic/Main.kt
@@ -0,0 +1,12 @@
+package vorobev.basic
+
+fun main() {
+    var name: String = "Ivan"
+    println(name)
+
+    // Error
+    // name = 25  ->
+
+    var nameAutoInitType = "Auto init to String type"
+    println(name)
+}

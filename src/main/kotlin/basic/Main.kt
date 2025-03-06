04424975fca5b8b889acaf17fae91ce14df417c6
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-03-07 02:34:02 +0300

    Update Main.kt

--- src/main/kotlin/basic/Main.kt
+++ src/main/kotlin/basic/Main.kt
@@ -2,11 +2,21 @@ package vorobev.basic
 
 fun main() {
     var name: String = "Ivan"
-    println(name)
+    println("Name: " + name)
 
     // Error
     // name = 25  ->
 
+    // val <==> final in Java
+
+    // !!! max use val
+
+    val autoMark = "BMW"
+
     var nameAutoInitType = "Auto init to String type"
-    println(name)
+    println("Name auto init String type: " + name)
+
+    var age: Int
+    age = 23
+    println("My age: " + age)
 }

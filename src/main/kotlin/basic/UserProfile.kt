ecd9aaac4a8f04c77bf2844b6ff5e9511e0c52db
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2025-03-07 15:48:46 +0300

    Update UserProfile.kt

--- src/main/kotlin/basic/UserProfile.kt
+++ src/main/kotlin/basic/UserProfile.kt
@@ -1,6 +1,18 @@
 package vorobev.basic
 
 fun main() {
-    println("Input your name: ")
-    readln()
+    println("Input your name:")
+    val name = readln()
+
+    println("Input your age:")
+    val age = readln()
+
+    println("Input your gender (M|W):")
+    val gender = readln()
+
+    println("Input your height:")
+    val height = readln()
+
+    println("Your name: " + name + ",\n your age: " + age + ",\n your gender: " + gender + ",\n your height: " + height)
+
 }
